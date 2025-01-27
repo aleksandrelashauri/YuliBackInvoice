bd8f11820fe4fc3a1d1cc432d54b5fdc0e020ae4
aleksandrelashauri <aleksandrelashauri@gmail.com>
2025-01-27 15:24:39 +0400

    combined invoice and voucher data handling

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ async function getBase64FromFile(filePath) {
 
 app.post("/generate-pdf", async (req, res) => {
 
-  const { invoiceData } = req.body;
+  const { invoiceData, documentType } = req.body;
 
   const browser = await puppeteer.launch({
     args: ["--allow-file-access-from-files", "--enable-local-file-accesses"],
@@ -79,13 +79,20 @@ app.post("/generate-pdf", async (req, res) => {
   try {
     // Generate PDFs for family groups
     for (let group of groupedHotels) {
+
       const printDate = group[0]?.createDate
         ? formatDate(excelDateToJSDate(group[0].createDate))
         : formatDate(new Date());
 
       const invoiceNumber = `${group[0]?.invoiceNumber || 'L0000'}${pdfPaths.length + 1}`;
 
-      const htmlContent = `
+      const htmlContent = documentType === 'invoice'
+        ? generateInvoiceTemplate(group, printDate, invoiceNumber, logo)
+        : generateVoucherTemplate(group, printDate, invoiceNumber, logo);
+
+
+      function generateInvoiceTemplate(group, printDate, invoiceNumber, logo) {
+        return `
 
       <!DOCTYPE html>
 <html lang="en">
@@ -233,14 +240,14 @@ app.post("/generate-pdf", async (req, res) => {
         </thead>
       <tbody>
             ${group
-          .map(
-            (member) => `
+            .map(
+              (member) => `
               <tr>
                 <td>${member.name}</td>
               </tr>
             `
-          )
-          .join("")}
+            )
+            .join("")}
           </tbody>
       </table>
       <table class="details-table">
@@ -258,9 +265,9 @@ app.post("/generate-pdf", async (req, res) => {
         </thead>
    <tbody>
             ${group
-          .filter(row => row.hotelCost > 0)
-          .map(
-            (row) => `
+            .filter(row => row.hotelCost > 0)
+            .map(
+              (row) => `
               <tr>
                 <td>wyndham batumi</td>
                 <td>${formatDate(excelDateToJSDate(row.startDate))}</td>
@@ -272,8 +279,8 @@ app.post("/generate-pdf", async (req, res) => {
                 <td>${row.total}</td>
               </tr>
             `
-          )
-          .join("")}
+            )
+            .join("")}
           </tbody>
       </table>
 
@@ -303,15 +310,240 @@ app.post("/generate-pdf", async (req, res) => {
   </body>
 </html>
     `;
+      }
+
+      function generateVoucherTemplate(group, printDate, invoiceNumber, logo) {
+        return ` 
+        
+      <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Voucher</title>
+    <style>
+    html {
+  -webkit-print-color-adjust: exact;
+}
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 20px;
+      }
+
+      .invoice {
+        width: 800px;
+        margin: auto;
+        padding: 20px;
+      }
+
+      /* Styles for the first table (Guest Names) */
+      .guest-table {
+        width: 100%;
+        margin-bottom: 6px;
+        border-collapse: collapse;
+      }
+      .guest-table th,
+      .guest-table td {
+        text-align: left;
+        border: none; 
+      }
+      .guest-table th {
+        background-color: #f2f2f2 !important;
+      }
+
+      /* Styles for the second table (Hotel Details) */
+      .details-table {
+        width: 100%;
+        /* margin-bottom: 20px; */
+        border-collapse: collapse;
+      }
+      .details-table th,
+      .details-table td {
+        border: 1px solid #333;
+        padding: 10px;
+        text-align: center;
+      }
+      .details-table th {
+        background-color: #d9e1f2 !important;
+      }
+
+      .total {
+        color: #002060;
+        border: 3px solid black;
+        padding: 3px;
+        display: flex;
+        justify-content: space-between;
+        /* text-align: right; */
+        font-weight: bold;
+        margin-top: 20px;
+      }
+
+      .signature-section {
+        color: #2f5496;
+        margin-top: 20px;
+      }
+      .left-line {
+        color: #2f5496;
+        text-align: right;
+        border-right: 8px solid #000;
+        padding: 10px;
+        height: 51px;
+        margin: 20px;
+        margin-right: 96px;
+      }
+      .padding_left {
+        padding-left: 137px;
+      }
+      .flex_box {
+        display: flex;
+        justify-content: space-between;
+        background-color: #b4c6e7 !important;
+        color: #1f4e78;
+        padding-top: 20px;
+        padding-right: 30px;
+      }
+      .bk_color {
+        color: #1f4e78;
+      }
+      .bk_color_guest {
+        background-color: #ddebf7 !important;
+      }
+      .bk_color_details {
+        background-color: #d9e1f2 !important;
+      }
+      .h3_style {
+        background-color: #ddebf7 !important;
+        color: #002060;
+        margin: 0;
+        padding-left: 15px;
+      }
+      .p_style {
+        margin-top: 3px;
+      }
+      .bank-details {
+        margin-top: 28px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="invoice">
+     <div><img src="data:image/jpeg;base64,${logo}" alt="logo" /</div>
+      <p>
+        Address: Batumi,Baratashvili Ave. 24, Phone: +995 568 93 36 92, ID:
+        445672736
+      </p>
+      <div class="left-line">
+        <div>Date ${printDate}</div>
+        <div>Invoice No ${invoiceNumber}</div>
+      </div>
+
+      <div class="flex_box" style="background-color: #b4c6e7 !important">
+        <div>
+          <div>Invoice No:</div>
+          <span class="bk_color">${invoiceNumber}</span>
+        </div>
+        <div>
+          <div>Agency Name:</div>
+          <span class="bk_color"> Yuli Tour</span>
+        </div>
+        <div>
+          <div>Print Date:</div>
+          <span class="bk_color"> ${printDate}</span>
+        </div>
+      </div>
+      <table class="guest-table">
+        <thead>
+          <tr>
+            <th class="bk_color_guest"   style="background-color: #ddebf7 !important" >Guest Names</th>
+          </tr>
+        </thead>
+      <tbody>
+            ${group
+            .map(
+              (member) => `
+              <tr>
+                <td>${member.name}</td>
+              </tr>
+            `
+            )
+            .join("")}
+          </tbody>
+      </table>
+      <table class="details-table">
+        <thead>
+          <tr class="bk_color_details">
+            <th>Hotel</th>
+            <th>Check In</th>
+            <th>Check Out</th>
+            <th>Nights</th>
+            <th>Room Type</th>
+            <th>Room Cost</th>
+            <th>Transfer Cost</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+   <tbody>
+            ${group
+            .filter(row => row.hotelCost > 0)
+            .map(
+              (row) => `
+              <tr>
+                <td>wyndham batumi</td>
+                <td>${formatDate(excelDateToJSDate(row.startDate))}</td>
+                <td>${formatDate(excelDateToJSDate(row.endDate))}</td>
+                <td>${row.nights}</td>
+                <td>${row.typeOfRoom}</td>
+                <td>${row.hotelPricesPerOneDay}</td>
+                <td>${row.transfer}</td>
+                <td>${row.total}</td>
+              </tr>
+            `
+            )
+            .join("")}
+          </tbody>
+      </table>
+      <h3 class="h3_style">Other Services</h3>
+      <p class="p_style">
+        All transfers by private car with English speaking driver
+      </p>
+        <div>
+            ${group
+            .filter(row => row.transfer > 0)
+            .map(
+              (row) => `
+                <p>${formatDate(excelDateToJSDate(row.startDate))} - transfer from airport </p>
+                <p>${formatDate(excelDateToJSDate(row.endDate))} - transfer to airport </p>
+            `
+            )
+            .join("")}
+        </div>
+      <div class="signature-section">
+        <p><strong>Authorized and Approved: Levan Davitadze</strong></p>
+        <p><strong>Print Name: Levan Davitadze</strong></p>
+        <p><strong>Print Date: ${printDate}</strong></p>
+      </div>
+    </div>
+  </body>
+</html>
+        
+        
+        `;
+      }
 
       const page = await browser.newPage();
+
       await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+
+      const fileName = documentType === 'invoice' ? 'Invoice' : 'Voucher';
+      const folderName = documentType === 'invoice' ? 'invoices' : 'vouchers';
 
       const pdfPath = path.join(
         __dirname,
-        "invoices",
-        `Invoice_${invoiceNumber}.pdf`
+        folderName,
+        `${fileName}_${invoiceNumber}.pdf`
       );
+
 
       await page.pdf({ path: pdfPath, format: "A4" });
       pdfPaths.push(pdfPath); // Store the path for sending later
